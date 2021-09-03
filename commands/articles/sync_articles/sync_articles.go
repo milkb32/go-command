2e3356b328d898c463e1fdaf0dc1b72ec463d9b7
华露 <hual@weipaitang.com>
2021-09-03 16:22:56 +0800

    modify

--- commands/articles/sync_articles/sync_articles.go
+++ commands/articles/sync_articles/sync_articles.go
@@ -6,6 +6,7 @@ import (
 	"github.com/milkb32/go-command/commands/articles/articles_mysql"
 	"github.com/milkb32/go-command/commands/articles/articles_es"
 	"github.com/urfave/cli"
+	"time"
 )
 
 // Init init cmds
@@ -22,6 +23,21 @@ func Init() {
 
 // Run run a cmd
 func Run() error {
+	i := 0
+	for ;i <= 100; {
+		err := RunOnce()
+		if err != nil {
+			fmt.Println("error:", err.Error())
+		}
+
+		time.Sleep(time.Duration(5) * time.Second)
+		i += 1
+	}
+
+	return nil
+}
+
+func RunOnce() error {
 	fmt.Println("start....")
 	// 获取es中最小的文章id
 	maxId, err := articles_es.GetMaxIdFromEs()
@@ -34,9 +50,17 @@ func Run() error {
 	if err != nil {
 		return err
 	}
+
+	if len(articles) == 0 {
+		fmt.Println("暂无新的文章，当前最大id:", maxId)
+		return nil
+	}
+
 	fmt.Println("同步文章数量：", len(articles))
 	// 将文章写入es
 	articles_es.SaveToEs(articles)
 
 	return nil
 }
+
+
